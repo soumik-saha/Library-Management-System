964a3a5ea78d7db4b8df3f60d7385128063c9bf5
Soumik Saha <sahasoumik1573@gmail.com>
2023-06-20 16:16:26 +0530

    adding css files

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,16 @@
-import { Button, Space } from 'antd';
+import './stylesheets/alignments.css';
+import './stylesheets/theme.css';
+import './stylesheets/sizes.css';
+import './stylesheets/form-elements.css';
+import './stylesheets/custom-components.css';
 
 function App() {
   return (
     <div>
-      <h1>
+      <h1 className="bg-primary flex justify-center items-center h-screen text-white">
         SOUS LIBRARY
       </h1>
 
-      <Space wrap>
-        <Button type="primary">Primary Button</Button>
-        <Button>Default Button</Button>
-        <Button type="dashed">Dashed Button</Button>
-        <Button type="text">Text Button</Button>
-        <Button type="link">Link Button</Button>
-      </Space>
     </div>
   );
 }
