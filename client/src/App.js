6a824042e7eae3d109b85b716ad2771e9fc411de
Soumik Saha <sahasoumik1573@gmail.com>
2023-06-20 18:02:35 +0530

    adding Login UI

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import './stylesheets/theme.css';
 import './stylesheets/sizes.css';
 import './stylesheets/form-elements.css';
 import './stylesheets/custom-components.css';
+import './pages/login'
+import LoginForm from './pages/login';
 
 function App() {
   return (
-    <div>
-      <h1 className="bg-primary flex justify-center items-center h-screen text-white">
-        SOUS LIBRARY
-      </h1>
+    <div className="bg-primary flex justify-center items-center h-screen text-white">
+
+      <LoginForm />
 
     </div>
   );
---
+++ client/src/pages/login.js
@@ -0,0 +1,71 @@
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Checkbox, Form, Input } from 'antd';
+import '../stylesheets/theme.css';
+
+const LoginForm = () => {
+    const onFinish = (values) => {
+        console.log('Received values of form: ', values);
+    };
+    return (
+        <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{
+                remember: true,
+            }}
+            onFinish={onFinish}
+        >
+            <h1>SOUMS LIBRARY</h1>
+
+            <Form.Item
+                name="username"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your Username!',
+                    },
+                ]}
+            >
+                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+            </Form.Item>
+            <Form.Item
+                name="password"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your Password!',
+                    },
+                ]}
+            >
+                <Input
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                    type="password"
+                    placeholder="Password"
+                />
+            </Form.Item>
+            <Form.Item>
+                <Form.Item name="remember" valuePropName="checked" noStyle>
+                    <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+
+                <a className="login-form-forgot" href="/">
+                    Forgot password
+                </a>
+            </Form.Item>
+            <Form.Item>
+                <div className='login-button'>
+                    <div>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            Log in
+                        </Button>
+                    </div>
+                    <div>
+                        Or <a href="/">register now!</a>
+                    </div>
+                </div>
+            </Form.Item>
+        </Form >
+    );
+};
+export default LoginForm;
+
